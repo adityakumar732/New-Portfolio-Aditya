01e93818a333a1ae4d6cd2222608a1176009c233
Aditya kumar <kumaranuj801503@gmail.com>
2024-11-24 21:50:00 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Portfolio from '../assets/Portfolio.png'
+import Portfoilo from '../assets/Portfoilo.png'
 import SpicyBites from '../assets/SpicyBites.png'
 import Netflix from '../assets/Netflix.png'
 import OYO from '../assets/OYO.jpeg'
@@ -13,45 +13,45 @@ const Projects = () => {
     const projectJson = [
         {
           title: 'Portfolio',
-          desc: 'Webelite Builders did an amazing job on our website. Their professionalism and dedication to our project were outstanding.',
-          image: Portfolio,
+          desc: 'My portfolio showcases my journey as a web developer, highlighting the skills and projects I’ve worked on.',
+          image: Portfoilo,
           live: "https://adityakumar732.github.io/New-Portfolio/",
-          github: "https://adityakumar732.github.io/New-Portfolio/"
+          github: "https://github.com/adityakumar732/New-Portfolio"
         },
         {
           title: 'Spicy Bites',
-          desc: 'The team at Webelite Builders exceeded our expectations with their digital marketing expertise. Highly recommend!',
+          desc: 'It is more than just food it’s an experience,Whether you're a spice enthusiast to explore exotic flavors.',
           image: SpicyBites,
           live: "https://spicybites.netlify.app/",
-          github: "https://github.com/rohitsingh93300/YtSpicyBites"
+          github: "https://github.com"
         },
         {
           title: 'Netflix Clone',
           desc: 'It is a streaming web page that offers TV shows, movies, anime, and more on thousands of Web series.',
           image: Netflix,
           live: "https://youtube-clone-93300.netlify.app/",
-          github: "https://github.com/rohitsingh93300/YouTube-clone"
+          github: "https://github.com/adityakumar732/Netflix"
         },
         {
           title: 'OYO Clone',
           desc: 'It is an online platform that enables users to find, and book the best hotels. Hotel offering a comfortable stay.',
           image: OYO,
-          live: "https://www.webelitebuilders.com/",
-          github: "#"
+          live: "https://adityakumar732.github.io/Oyo/",
+          github: "https://github.com/adityakumar732/Oyo"
         },
         {
           title: 'Tesla Car',
           desc: 'The menu and homepage highlight core offerings, especially the cars, rather than delving into corporate details.',
           image: Tesla,
-          live: "https://supercar123.netlify.app/",
-          github: "https://github.com/rohitsingh93300/supercars"
+          live: "https://adityakumar732.github.io/tesla/",
+          github: "https://github.com/adityakumar732/tesla"
         },
         {
           title: 'Tic Tac Toe Game',
           desc: ' It is a game in which the objective is to mark the correct spaces in a 3x3. Align three symbols horizontally, vertically, or diagonally to win.',
           image: Game,
-          live: "https://supercar123.netlify.app/",
-          github: "https://github.com/rohitsingh93300/supercars"
+          live: "https://adityakumar732.github.io/New-tic-toe/",
+          github: "https://github.com/adityakumar732/New-tic-toe"
         },
       ];
 
