b6f2871739b2d70029751872eee3778a0a7857a7
Aditya kumar <kumaranuj801503@gmail.com>
2024-11-25 13:28:51 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -62,7 +62,7 @@ const Hero = () => {
               <span className="blinking-cursor">|</span>
             </p>
             <p className='mb-4'>I'm a passionate Web Developer with expertise in React.js, Express.js, and modern web technologies. I love creating beautiful and functional website that solve real world problems.</p>
-            <button className='bg-black text-white px-3 py-2 w-max rounded-md hover:bg-red-500 '><a href="https://drive.google.com/uc?export=download&id=1H5YaYCE8h3zNvzYaPrUebDcfKhlYBrVD" download target='_blank'>Download CV</a></button>
+            <button className='bg-black text-white px-3 py-2 w-max rounded-md hover:bg-red-500 '><a href="https://drive.google.com/uc?export=download&id=1IR1i7pW9aOVkMjzpPEWDhFjHq8gSBbjK" download target='_blank'>Download CV</a></button>
           </div>
           <div className='md:w-1/2 relative flex justify-center items-end'>
             <img src={hero} alt="" className='lg:h-[90vh] h-96' />
