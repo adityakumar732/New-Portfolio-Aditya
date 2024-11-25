f585792144d06b23504143ba5d3102963c9d2ef5
Aditya kumar <kumaranuj801503@gmail.com>
2024-11-25 12:46:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/New-Portfolio-Aditya/', // Replace 'your-repo-name' with your GitHub repository name
+});
