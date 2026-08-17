757b1a560cc93c4e086d5479ba28882edbd0b912
Aditya kumar <kumaranuj801503@gmail.com>
2026-08-18 00:17:32 +0530

    Remove Netflix, OYO, and Tesla project entriesprojects removed
    Removed several project entries from the Projects component.

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -25,27 +25,27 @@ const Projects = () => {
           live: "https://spicybites.netlify.app/",
           github: "https://github.com"
         },
-        {
-          title: 'Netflix Clone',
-          desc: 'It is a streaming web page that offers TV shows, movies, anime, and more on thousands of Web series.',
-          image: Netflix,
-          live: "https://youtube-clone-93300.netlify.app/",
-          github: "https://github.com/adityakumar732/Netflix"
-        },
-        {
-          title: 'OYO Clone',
-          desc: 'It is an online platform that enables users to find, and book the best hotels. Hotel offering a comfortable stay.',
-          image: OYO,
-          live: "https://adityakumar732.github.io/Oyo/",
-          github: "https://github.com/adityakumar732/Oyo"
-        },
-        {
-          title: 'Tesla Car',
-          desc: 'The menu and homepage highlight core offerings, especially the cars, rather than delving into corporate details.',
-          image: Tesla,
-          live: "https://adityakumar732.github.io/tesla/",
-          github: "https://github.com/adityakumar732/tesla"
-        },
+        // {
+        //   title: 'Netflix Clone',
+        //   desc: 'It is a streaming web page that offers TV shows, movies, anime, and more on thousands of Web series.',
+        //   image: Netflix,
+        //   live: "https://youtube-clone-93300.netlify.app/",
+        //   github: "https://github.com/adityakumar732/Netflix"
+        // },
+        // {
+        //   title: 'OYO Clone',
+        //   desc: 'It is an online platform that enables users to find, and book the best hotels. Hotel offering a comfortable stay.',
+        //   image: OYO,
+        //   live: "https://adityakumar732.github.io/Oyo/",
+        //   github: "https://github.com/adityakumar732/Oyo"
+        // },
+        // {
+        //   title: 'Tesla Car',
+        //   desc: 'The menu and homepage highlight core offerings, especially the cars, rather than delving into corporate details.',
+        //   image: Tesla,
+        //   live: "https://adityakumar732.github.io/tesla/",
+        //   github: "https://github.com/adityakumar732/tesla"
+        // },
         {
           title: 'Tic Tac Toe Game',
           desc: ' It is a game in which the objective is to mark the correct spaces in a 3x3. Align three symbols horizontally, vertically, or diagonally to win.',
